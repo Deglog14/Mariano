fdf33c1b160b630c1b5741eec49db4a6010b7aa1
DESKTOP-G387TLV\Tarde <ponixsad@gmail.com>
2023-12-04 19:33:08 +0100

    muerte finalizada y empiece de plataforma que se mueve

--- Assets/Scripst/Player_Life.cs
+++ Assets/Scripst/Player_Life.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player_Life : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Trampa"))
+        {
+            Muerto(); 
+        }
+    }
+    private void Muerto()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("muerte");
+    }
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
